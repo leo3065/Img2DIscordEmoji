19ecb3b285c83a0e5ae98fe474e868aacbd0fe0c
leo3065 <leo3065@gmail.com>
2023-02-23 00:58:49 +0800

    rework GUI layout

--- img2discord_emoji_gui.py
+++ img2discord_emoji_gui.py
@@ -2,30 +2,31 @@ from fractions import Fraction
 from pathlib import Path
 import os
 import tkinter as tk
+import tkinter.filedialog as tkFileDialog
 
 from PIL import Image
 
 from typing import Tuple
 
 
-def as_interger_ratio(val: float, max_min_term: int = None) -> Tuple[int]:
-    if val > 1:
-        frac = Fraction(val)
+def get_tile_size(aspect_ratio: float, tile_size: Tuple[int, int]) -> Tuple[int, int]:
+    if tile_size == (0, 0):
+        if aspect_ratio > 1:
+            return (round(aspect_ratio), 1)
+        else:
+            return (1, round(1/aspect_ratio))
+    elif tile_size[0] == 0:
+        return (round(aspect_ratio*tile_size[1]), tile_size[1])
+    elif tile_size[1] == 0:
+        return (tile_size[0], round(aspect_ratio*tile_size[0]))
     else:
-        frac = Fraction(1/val)
-    if max_min_term is not None:
-        frac = frac.limit_denominator(max_min_term)
-    if val > 1:
-        return frac.as_integer_ratio()
-    else:
-        return (1/frac).as_integer_ratio()
+        return tile_size
 
 
 def main(
         in_filename: str, out_filename: str,
         keep_boarder: bool = False, max_tile_size: int = 1):
-    img = Image.open(in_filename)
-    img = img.convert('RGBA')
+    
 
     if not keep_boarder:
         img = img.crop(img.getbbox())
@@ -90,47 +91,86 @@ class App(tk.Tk):
         self.image = None
     
     def setup_widgets(self):
-        app_frame = tk.Frame(self)
+        app_frame = self.app_frame = tk.Frame(self)
         app_frame.grid(row=0, column=0, padx=5, pady=5)
         
         ## Row 0
-        button_select_image = self.button_select_image = tk.Button(
-            app_frame, text='Select image:')
+        button_select_image = app_frame.button_select_image = tk.Button(
+            app_frame, text='Select image', command=self.load_image)
         button_select_image.grid(
             row=0, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
-        label_in_path = app_frame.label_in_path = tk.Label(
-            app_frame, text='', relief=tk.SUNKEN, width=50, anchor=tk.NW)
-        label_in_path.grid(
+        
+        var_keep_boarder = app_frame.var_keep_boarder = tk.IntVar()
+        checkbutton_keep_boarder = app_frame.checkbutton_keep_boarder = tk.Checkbutton(
+            app_frame, text='Keep transparent border', variable=var_keep_boarder, anchor=tk.NW)
+        checkbutton_keep_boarder.grid(
             row=0, column=1, columnspan=2, sticky=tk.W+tk.E, padx=5, pady=5)
         
         ## Row 1
-        button_select_out_path = self.button_select_out_path = tk.Button(
-            app_frame, text='Select output folder:')
-        button_select_out_path.grid(
-            row=1, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
-        label_out_path = app_frame.label_out_path = tk.Label(
-            app_frame, text='', relief=tk.SUNKEN, width=50, anchor=tk.NW)
-        label_out_path.grid(
-            row=1, column=1, columnspan=2, sticky=tk.W+tk.E, padx=5, pady=5)
-        
-        ## Row 2
         tk.Label(
             app_frame, text='Output size (0=auto):'
-        ).grid(row=2, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
+        ).grid(row=1, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
+        
         spinbox_width = app_frame.spinbox_width = tk.Spinbox(
-            app_frame, from_=0, to_=10, increment=1, justify=tk.CENTER, state='readonly')
+            app_frame, from_=0, to_=10, increment=1, 
+            justify=tk.CENTER, state='readonly', width=5)
         spinbox_width.grid(
-            row=2, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
+            row=1, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
+        
         spinbox_height = app_frame.spinbox_height = tk.Spinbox(
-            app_frame, from_=0, to_=10, increment=1, justify=tk.CENTER, state='readonly')
+            app_frame, from_=0, to_=10, increment=1, 
+            justify=tk.CENTER, state='readonly', width=5)
         spinbox_height.grid(
-            row=2, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
+            row=1, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
+        
+        ## Row 2
+        label_img_info = app_frame.label_img_info = tk.Label(
+            app_frame, text='No image loaded', height=6, relief=tk.GROOVE, anchor=tk.NW, justify=tk.LEFT)
+        label_img_info.grid(row=2, column=0, columnspan=3, sticky=tk.W+tk.E, padx=5, pady=5)
         
         ## Row 3
-        label_img_info = self.label_img_info = tk.Label(
-            app_frame, text='No image loaded', height=4, relief=tk.GROOVE, anchor=tk.NW)
-        label_img_info.grid(row=3, column=0, columnspan=3, sticky=tk.W+tk.E, padx=5, pady=5)
-
+        button_convert = app_frame.button_convert = tk.Button(
+            app_frame, text='Convert', state=tk.DISABLED)
+        button_convert.grid(
+            row=3, column=0, columnspan=3, sticky=tk.W+tk.E, padx=5, pady=5)
+    
+    def load_image(self):
+        in_filename = tkFileDialog.askopenfilename(
+            initialdir=os.getcwd(),
+            filetypes=[('Image', '.png .jpg .jpeg .gif')])
+        if not in_filename:
+            return
+        
+        app_frame = self.app_frame
+        
+        image = self.image = Image.open(in_filename).convert('RGBA')
+        self.update_image_info()
+        
+        app_frame.button_convert.config(state=tk.ACTIVE)
+    
+    def update_image_info(self):
+        if not self.image:
+            return
+        app_frame = self.app_frame
+        
+        info_lines = []
+        
+        image = self.image
+        info_lines.append(f'Image dimension: {image.size[0]} x {image.size[1]}')
+        if not app_frame.var_keep_boarder.get():
+            if bbox := image.getbbox():
+                image = image.crop(bbox)
+                info_lines.append(f'After removing border: {image.size[0]} x {image.size[1]}')
+        
+        aspect_ratio = image.size[0]/image.size[1]
+        info_lines.append(f'Aspect ratio: {aspect_ratio:.4f} (1/{1/aspect_ratio:.4f})')
+        
+        tile_width = int(app_frame.spinbox_width.get())
+        tile_height = int(app_frame.spinbox_height.get())
+        tile_width, tile_height = get_tile_size(aspect_ratio, (tile_width, tile_height))
+        info_lines.append(f'Output size: {tile_width} x {tile_height} = {tile_width*tile_height}')
+        
+        app_frame.label_img_info.config(text='\n'.join(info_lines))
 
 
 if __name__ == '__main__':
